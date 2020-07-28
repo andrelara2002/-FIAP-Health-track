0e53984b7f9e7628f77cb3cb7ae35c252e877695
andrelara2002 <andrelara2002@gmail.com>
2020-07-28 14:40:33 -0300

    autenticação provisória aplicada

--- src/login.js
+++ src/login.js
@@ -1,11 +1,24 @@
 const loginElement = document.getElementById('email');
 const passElement = document.getElementById('password')
 
+let users = {
+    usuario: 'andrelara',
+    senha: 'fiap1234'
+}
+
 document.querySelector('button').onclick = function(){
     if (loginElement.value === ''){
-        alert.apply('Por favor, insira seu login')
+        alert('Por favor, insira seu login')
     }
     else if (passElement.value === ''){
         alert('Por favor, insira sua senha')
+    }
+    else{
+        if (passElement.value === users.senha && loginElement.value === users.usuario){
+            alert('Autenticação realizada')
+        }
+        else{
+            alert('Usuário ou senha incorreta')
+        }
     }
 }
