3b3ca061fad00263b833c8eb78fe237893676a6b
andrelara2002 <andrelara2002@gmail.com>
2020-07-28 18:03:10 -0300

    Utilizando javascript para uma lista interativa

---
+++ src/esportes.js
@@ -0,0 +1,26 @@
+const esportes = ['Natação', 'Ciclismo', 'Caminhada', 'Artes Marciais', 'Atletismo', 'Outros']
+const esporteElement = document.getElementById('divEsportes');
+
+render();
+
+function render() {
+    esporteElement.innerHTML = '';
+
+    for (esporte of esportes) {
+        const textElement = document.createTextNode(esporte);
+        const nameElement = document.createElement('div')
+        nameElement.setAttribute('class', 'shadow-sm card w-100')
+        nameElement.setAttribute('style', 'margin-top: 10px;')
+
+        const lastDiv = document.createElement('div')
+        lastDiv.setAttribute('class', 'card-body">')
+
+        const finalText = document.createElement('h1')
+        finalText.appendChild(textElement)
+
+        lastDiv.appendChild(finalText)
+        nameElement.appendChild(lastDiv);
+
+        esporteElement.appendChild(nameElement)
+    }
+}
